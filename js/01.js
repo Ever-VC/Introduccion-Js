12ea03ca89c12e3cf7e597577f190f7fc3279637
Ever-VC <ever.vasquezc27@gmail.com>
2024-09-16 08:11:22 -0600

    ✨ New Feature: Variables var en js.

---
+++ js/01.js
@@ -0,0 +1,29 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true; // Asignando valor a la variable
+
+// Iniciar varias variables a la vez
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Las variables no pueden iniciar con numeros
+// var 99dias;
+var dias99;
+
+// Las variables no pueden tener guiones medios
+// var nombre-producto;
+
+// Estilos para variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD'; // Lower Case
+
+console.log(producto); // Mostrar en consola
