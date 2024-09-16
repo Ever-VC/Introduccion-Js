df3e22f4580f4bbcb1ae5316202b3a312d379084
Ever-VC <ever.vasquezc27@gmail.com>
2024-09-16 08:22:35 -0600

    ✨ New Feature: Variables let en js.

---
+++ js/02.js
@@ -0,0 +1,27 @@
+// Variables
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true; // Asignando valor a la variable
+
+// Iniciar varias variables a la vez
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Las variables no pueden iniciar con numeros
+// let 99dias;
+let dias99;
+
+// Las variables no pueden tener guiones medios
+// let nombre-producto;
+
+// Estilos para variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; // Camelcase
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreproducto = 'Monitor HD'; // Lower Case
