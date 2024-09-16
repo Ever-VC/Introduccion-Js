ec101bac8a53748217d56eed6579cbd29ada0930
Ever-VC <ever.vasquezc27@gmail.com>
2024-09-16 08:27:32 -0600

    ✨ New Feature: Variables const en js.

---
+++ js/03.js
@@ -0,0 +1,27 @@
+// Variables
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible = true; // No se puede iniciar una variable sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true; // No se puede reasignar una constante
+
+// Iniciar varias variables a la vez
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Las variables no pueden iniciar con numeros
+// const 99dias;
+const dias99 = 99;
+
+// Las variables no pueden tener guiones medios
+// const nombre-producto;
+
+// Estilos para variables
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD'; // Camelcase
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreproducto = 'Monitor HD'; // Lower Case
