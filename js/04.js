3e494d05c67c8054952745130f9ab168bb268790
Ever-VC <ever.vasquezc27@gmail.com>
2024-09-16 08:33:35 -0600

    ✨ New Feature: Introducción a los Strings

---
+++ js/04.js
@@ -0,0 +1,8 @@
+//Strings o cadenas de texto
+const producto = 'Monitor de 24"';
+const producto2 = String("Monitor de 19\"");
+const producto3 = new String('Monitor de 50 pulgadas');
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
