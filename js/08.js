9a6c8d0ceb1a61c5b3b821d1c40cc81f220f33d4
Ever-VC <ever.vasquezc27@gmail.com>
2024-10-29 15:53:43 -0600

    ✨ New Feature: Concatenación y Template Strings

---
+++ js/08.js
@@ -0,0 +1,8 @@
+const nombre = 'Ever';
+const email = 'correo@correo.com';
+
+// Concatenación
+console.log(nombre + " " + email);  // Ever correo@correo.com
+
+// Template Strings
+console.log(`Nombre Cliente: ${nombre}, Email: ${email}`);  // Nombre Cliente: Ever, Email: correo@correo.com
