c57243eeb48051563dce88d3f7fc144d55e3d50a
Ever-VC <ever.vasquezc27@gmail.com>
2024-10-29 15:34:08 -0600

    ✨ New Feature: Objeto math

---
+++ js/06.js
@@ -0,0 +1,16 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5);
+resultado = Math.ceil(2.2); // Redondea hacia arriba
+resultado = Math.floor(2.9); // Redondea hacia abajo
+resultado = Math.sqrt(144); // Raiz cuadrada
+resultado = Math.abs(-200); // Valor absoluto
+resultado = Math.min(3, 5, 1, 8, 2, 10); // Valor mínimo
+resultado = Math.max(3, 5, 1, 8, 2, 10); // Valor máximo
+resultado = Math.random(); // Número aleatorio entre 0 y 1
+resultado = Math.floor(Math.random() * 30); // Número aleatorio entre 0 y 30
+
+console.log(resultado);
