531cff1a883450ecb5a761c119b090b875117011
Ever-VC <ever.vasquezc27@gmail.com>
2024-10-29 15:43:47 -0600

    ✨ New Feature: Orden de operadores, incrementos y decrementos

---
+++ js/07.js
@@ -0,0 +1,24 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2; // 80
+resultado = (20 + 30) * 2; // 100
+
+resultado = (600 + 600) * 1.21; // 1452
+
+//console.log(resultado);
+
+// Incrementos
+
+let puntaje = 10;
+puntaje ++; // 11
+console.log(puntaje++); // 11
+console.log(++puntaje); // 13
+
+puntaje --; // 12
+
+puntaje += 10; // 22
+
+
+console.log(puntaje);
