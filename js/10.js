d2ffc05a39672939091084bca7d39eb85326b412
Ever-VC <ever.vasquezc@gmail.com>
2024-11-27 20:39:15 -0600

    ✨ New Feature: Objetos en JS

---
+++ js/10.js
@@ -0,0 +1,24 @@
+// Objetos
+
+const nombre_producto = "Monitor 20 Pulgadas";
+const precio = 300;
+const disponible = true;
+
+// Un objeto agrupa todo en una sola variable
+const producto = {
+    nombre_producto: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+console.log(producto);
+
+// Acceder a los valores de un objeto
+console.log(producto.nombre_producto);
+console.log(producto.precio);
+console.log(producto.disponible);
+
+// Otra forma de acceder a los valores de un objeto
+console.log(producto['nombre_producto']);
+console.log(producto['precio']);
+console.log(producto['disponible']);
