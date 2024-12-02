2262157cff524f7020cfb69825450cf79bb93977
Ever-VC <ever.vasquezc@gmail.com>
2024-12-02 12:49:36 -0600

    ✨ New Feature: Métodos de arreglos

--- js/14.js
+++ js/14.js
@@ -26,3 +26,21 @@ for(let i = 0; i < numeros.length; i++) {
     console.log(numeros[i]);
 }
 
+// Agregar elementos a un arreglo
+numeros[5] = 60;
+numeros.push(70);
+numeros.push(80, 90, 100);
+
+// Agregar al inicio
+numeros.unshift(-30, -20, -10);
+console.table(numeros);
+
+// Eliminar elementos de un arreglo
+numeros.pop();// Elimina el ultimo elemento
+numeros.shift();// Elimina el primer elemento
+meses.splice(2, 1);// Elimina un elemento en la posicion 2 del arreglo meses y solo un elemento
+console.table(numeros);
+
+// Rest Operator o Spread Operator
+const nuevoArreglo = [...meses, "Junio"];
+console.table(nuevoArreglo);
