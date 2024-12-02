a0b2a94c07f1070d24656cfffaf2066bca9135da
Ever-VC <ever.vasquezc@gmail.com>
2024-12-02 13:53:16 -0600

    ✨ New Feature:  Diferencia entre funciones y métodos

---
+++ 17.js
@@ -0,0 +1,9 @@
+const numero1 = 20;
+const numero2 = 20;
+
+console.log(parseInt(numero1)); // ParseInt es una función que convierte un string a un número entero
+console.log(numero1.toString()); // toString es un método que convierte un número a un string
+
+// La diferencia entre una función y un método es que la función se invoca de la 
+//siguiente manera: nombreFuncion(parametro) y el método se invoca de la 
+//siguiente manera: nombreVariable.nombreMetodo(parametro)
