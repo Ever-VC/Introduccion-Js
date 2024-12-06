d6e4c2b4a997c167e3f61a7d4a9f3380a6ad03ee
Ever-VC <ever.vasquezc@gmail.com>
2024-12-05 20:12:41 -0600

    ✨ New Feature: Arrow functions

---
+++ js/19.js
@@ -0,0 +1,17 @@
+// Arrow functions
+const surmar2 = function(n1, n2) {
+    console.log(n1 + n2);
+}
+
+const sumar = (n1, n2) => {
+    console.log(n1 + n2);
+}
+
+const restar = (n1, n2) => console.log(n1 - n2);
+
+const aprendiendo = tecnologia => console.log(`Aprendiendo ${tecnologia}`);
+
+surmar2(2, 4);
+sumar(2, 4);
+restar(12, 10);
+aprendiendo('JavaScript');
