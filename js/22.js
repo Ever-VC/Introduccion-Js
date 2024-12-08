6032f0dd9df6e8488b9e9844b74a2963c42216b6
Ever-VC <ever.vasquezc@gmail.com>
2024-12-08 14:31:16 -0600

    ✨ New Feature: While y Do While en js

--- js/22.js
+++ js/22.js
@@ -24,10 +24,46 @@ const carrito = [
     {nombre: 'Impresora', precio: 350}
 ];
 
-for (let i = 0; i < carrito.length; i++) {
+/*for (let i = 0; i < carrito.length; i++) {
     console.log(carrito[i].nombre);
-}
+}*/
 
 //While loop
+let i = 0;
+
+/*while (i < carrito.length) {
+    console.log(carrito[i].nombre);
+    i++;
+}*/
+
+i = 0;
+//i = 20;
+
+/*while (i < 10) {
+    if (i % 2 == 0) {
+        console.log(`El número ${i} es par`);
+    } else {
+        console.log(`El número ${i} es impar`);
+    }
+    i++;
+}*/
 
 //Do while loop
+i = 0;
+//i = 20;
+
+do {
+    if (i % 2 == 0) {
+        console.log(`El número ${i} es par`);
+    } else {
+        console.log(`El número ${i} es impar`);
+    }
+    i++;
+} while (i < 10);
+
+i = 0;
+
+do {
+    console.log(carrito[i].nombre);
+    i++;
+} while (i < carrito.length);
